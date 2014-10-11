86cc24a1a07c665cc28e9e7306f89518b5353e23
Artiom <tjomamokrenko@gmail.com>
2014-10-11 23:49:38 +0300

    Добавлена проверка переменных сессии

--- system/auth.php
+++ system/auth.php
@@ -2,11 +2,7 @@
 
 session_start();
 
-$result = $mysqli->query("SELECT `session_id` FROM `sessions` WHERE `php_session_id` = '" . $mysqli->real_escape_string(session_id()) . "' AND `user_id` = " . (int) $_SESSION['id'] . " AND `expires` > NOW()");
-
-if ($result->num_rows == 0) {
-	$result->free();
-
+function sign_out() {
 	session_unset();
 
 	if (ini_get("session.use_cookies")) {
@@ -20,12 +16,26 @@ if ($result->num_rows == 0) {
 
 	session_destroy();
 
-	header('Location: http://' . $_SERVER['HTTP_HOST'] . '/login.php');
+	return header('Location: http://' . $_SERVER['HTTP_HOST'] . '/login.php');
+}
+
+if (!empty($_SESSION) && !empty($_SESSION['id']) && !empty($_SESSION['login']) && !empty($_SESSION['password']) && $_SESSION['ip'] == $_SERVER['REMOTE_ADDR']) {
+	$result = $mysqli->query("SELECT `session_id` FROM `sessions`
+									WHERE `php_session_id` = '" . $mysqli->real_escape_string(session_id()) . "'
+									AND `user_id` = " . (int) $_SESSION['id'] . "
+									AND `expires` > NOW()");
+
+	if ($result->num_rows == 0) {
+		$result->free();
+		sign_out();
+	} else {
+		$mysqli->query("UPDATE `sessions`
+							SET `expires` = NOW() + INTERVAL 60 MINUTE
+							WHERE `user_id` = " . (int) $_SESSION['id'] . "
+							LIMIT 1");
+	}
 } else {
-	$mysqli->query("UPDATE `sessions`
-						SET `expires` = NOW() + INTERVAL 60 MINUTE
-						WHERE `user_id` = " . (int) $_SESSION['id'] . "
-						LIMIT 1");
+	sign_out();
 }
 
 ?>
