fd9587f30423def042f33d35de5c3ee3d71f1f4c
Ruslaner <ruslaner512@yandex.ru>
2014-10-12 15:45:38 +0300

    Центрирование формы, полосы прокрутки, кнопки

--- view/chat.php
+++ view/chat.php
@@ -5,16 +5,15 @@
 		<meta charset="utf-8">
 		<title>Chat</title>
 		<link rel="stylesheet" href="styles.css">
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>	
 
 	</head>
 
 	<body>
 
-		<div class="window">
-
-			<div class="header">
-            Мастер Йода <input type="radio" name="online" disabled><label for=""><span></span></label>
-        </div>
+			<div class="window">
+				<div class="header">Мастер Йода <input type="radio" name="online" disabled><label for=""><span></span></label>
+	        </div>
 			
 			<div class="lsidebar">
 				<div class="people">Участники</div>				
@@ -96,10 +95,20 @@
 							</div>
 							<div class="mess">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent diam augue, pellentesque id lorem quis, aliquam commodo elit. Aenean sed posuere lectus. Praesent sagittis purus nec purus iaculis facilisis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer nec tortor odio.</div>
 						</div>
+							<div class="msgLeft">
+							<div class="name">
+								<span class="msgName">Артём Мокренко</span>
+								<span class="msgTime">14:53</span>
+								<span  class="mstTime2">Прочитано: 14:54</span>
+							</div>
+							<div class="mess">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent diam augue, pellentesque id lorem quis, aliquam commodo elit. Aenean sed posuere lectus. Praesent sagittis purus nec purus iaculis facilisis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer nec tortor odio.</div>
+						</div>
+						
 						
 					</div>
 				</div>
 				<div class="text">
+
 					<div class="innerarea">
 						<textarea placeholder="Введите сообщение.." class="area"></textarea>
 						<table class="messageButtons">
@@ -113,6 +122,7 @@
 					</div>
 				</div>
 			</div>
-		</div>	
+		</div>
+
 	</body>
 </html>
