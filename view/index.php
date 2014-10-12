e85b2f517c8d553367321f3c7163863b342e9385
Ruslaner <ruslaner512@yandex.ru>
2014-10-12 11:14:53 +0300

    Открытие чата с заданными размерами

---
+++ view/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Index</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<!--<div class="header"><span>Главная страница</span><div class="profile"><span><div class="avatar"></div>Мокренко Артём</span></div></div>
+	<div class="sidebar">
+		<ul class="menu">
+			<li>Главная страница</li>
+			<li>Сообщения
+
+			</li>
+			<li>Задачи
+				<ul>
+					<li>
+						<ul>
+							<li>Выполненные</li>
+							<li>Просроченные</li>
+							<li>Для согласования</li>
+							<li>Периодические</li>
+							<li>Завершенные</li>
+						</ul>
+					</li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</li>
+			<li>Архив документов</li>
+			<li>Потенциальные клиенты</li>
+			<li>Отчёты</li>
+			<li>Пользователи</li>
+			<li>Выход</li>
+
+		</ul>
+	</div>-->
+	<button onclick="chatOpen()">Chat</button>
+	<script>
+	function chatOpen() {
+    	window.open("chat.php", "_blank", "toolbar=yes, scrollbars=yes, resizable=yes,width=800, height=700");
+	}
+</script>
+</body>
+</html>
